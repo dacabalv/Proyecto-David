214e3f492213161f3f69cf8f66f288539972c0b0
David <david@example.com>
2015-11-18 14:17:28 +0100

    Miercoles 17 Nov

--- Proyecto_David/Viajar/urls.py
+++ Proyecto_David/Viajar/urls.py
@@ -3,4 +3,7 @@ from Viajar import views
 
 urlpatterns = [
    url(r'^$', views.indice, name='indice'),
+	url(r'^registro$', views.registro, name='registro'),
+	url(r'^login$', views.loginpage, name='login'),
+	url(r'^logout$', views.logoutpage, name='logout'),
 ]
