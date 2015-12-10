ef7b261676cb19bf0570f923173acfdf3e6b3fe4
David <david@example.com>
2015-12-10 10:21:24 +0100

    Jueves 10/2 Diciembre

--- Proyecto_David/Viajar/urls.py
+++ Proyecto_David/Viajar/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
    url(r'^$', views.indice, name='indice'),
 	url(r'^ediciones/(?P<viaje_id>\d+)/$',views.ediciones, name='ediciones'), #a esta url le doy el nombre de ediciones.
 	url(r'^seleccion/(?P<edicion_id>\d+)/$',views.selecciones, name='selecciones'), # a esta la llamo selecciones , para la edicion que eligen.
+	url(r'^reserva_usuario$',views.reservas_usuario, name='reservas_usuario'),
 	url(r'^registro$', views.registro, name='registro'),
 	url(r'^login$', views.loginpage, name='login'),
 	url(r'^logout$', views.logoutpage, name='logout'),
--- Proyecto_David/Viajar/views.py
+++ Proyecto_David/Viajar/views.py
@@ -3,7 +3,9 @@ from Viajar.models import Viaje,Edicion #aquí importo los del models.
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login,logout,authenticate
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+#Aquí en el views hago las consultas sobre el models y se lo paso a los html.
 
 def indice(request):
    Viajes = Viaje.objects.all()
@@ -20,6 +22,7 @@ def registro(request):
 	return render(request, "Viajar/registro.html", {
 'form': form,})
 
+@login_required
 def loginpage(request):
 	if request.method == 'POST':
 		form = AuthenticationForm(request.POST)
@@ -34,6 +37,7 @@ def loginpage(request):
 			form = AuthenticationForm()
 	return render (request, "Viajar/login.html", {'form': form,})
 
+@login_required
 def logoutpage(request):
 	logout(request)
 	return HttpResponseRedirect("/")
@@ -42,6 +46,7 @@ def ediciones(request,viaje_id): #viaje_id es la clave primaria que te crea djan
 	ediciones = Edicion.objects.filter(viaje=viaje_id) #De todas las ediciones, que me coja solamente las del viaje que selecciono.
 	return render (request, 'Viajar/ediciones.html', {'ediciones': ediciones,})	#con esto le digo que me lleve al html(me lo saca en la pagina)
 
+@login_required
 def selecciones(request,edicion_id):
 	edicion = get_object_or_404(Edicion, pk = edicion_id)	
 	edicion.usuarios.add(request.user)
@@ -50,5 +55,11 @@ def selecciones(request,edicion_id):
 	reservas = Edicion.objects.filter(usuarios__in=[request.user.id])
 	return render (request, 'Viajar/reserva.html', {'reservas': reservas,})
 
+@login_required
+def reservas_usuario(request):
+	reservas = Edicion.objects.filter(usuarios__in=[request.user.id])
+	return render (request, 'Viajar/reserva.html', {'reservas': reservas,})
 
+	
+	
 
